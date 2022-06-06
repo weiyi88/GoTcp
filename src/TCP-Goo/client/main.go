4f04a538002dfbe54b0adb4b0830d655239932ee
Aring <tantubuping@163.com>
2022-06-07 00:58:54 +0800

    先完全使用GOPATH，GO111MODEL=off

--- src/TCP-Goo/client/main.go
+++ src/TCP-Goo/client/main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"fmt"
 	"net"
+	"protocol"
 	"strconv"
 	"time"
 )
 
 func main() {
+	protocol.Test()
+	return
 	connect, err := net.Dial("tcp", "127.0.0.1:30000")
 	if err != nil {
 		fmt.Println("dail failed:", err)
@@ -16,6 +19,9 @@ func main() {
 	defer connect.Close()
 	for i := 0; i < 20; i++ {
 		msg := `Hello 这是第 ` + strconv.Itoa(i) + "条信息"
+
+		// 调用协议 编码数据
+		protocol.Test()
 		time.Sleep(time.Second)
 		connect.Write([]byte(msg))
 	}
