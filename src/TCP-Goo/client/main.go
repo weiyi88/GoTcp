034ad12adb94641610f3c0beb58b27d046d4cd23
lry <liurongye@inke.cn>
2022-07-15 11:21:08 +0800

    删除多余字段

--- src/TCP-Goo/client/main.go
+++ src/TCP-Goo/client/main.go
@@ -3,14 +3,10 @@ package main
 import (
 	"fmt"
 	"net"
-	"protocol"
 	"strconv"
-	"time"
 )
 
 func main() {
-	protocol.Test()
-	return
 	connect, err := net.Dial("tcp", "127.0.0.1:30000")
 	if err != nil {
 		fmt.Println("dail failed:", err)
@@ -21,8 +17,6 @@ func main() {
 		msg := `Hello 这是第 ` + strconv.Itoa(i) + "条信息"
 
 		// 调用协议 编码数据
-		protocol.Test()
-		time.Sleep(time.Second)
 		connect.Write([]byte(msg))
 	}
 }
